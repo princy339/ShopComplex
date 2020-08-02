8e8063cb2fd679a050d286777ed32d940e03c942
princy agrawal <princygarg42@gmail.com>
2020-08-02 20:29:46 +0530

    work in log in component

--- shopingcomplex/src/app/app-routing.module.ts
+++ shopingcomplex/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'home',component:HomeComponent},
+                        {path:'header',component:HeaderComponent},
+                        {path:'about',component:AboutComponent},
+                        {path:'login',component:LoginComponent},
+                        {path:'signup',component:SignupComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shopingcomplex/src/app/app.module.ts
+++ shopingcomplex/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
